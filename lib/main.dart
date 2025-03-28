b2e611f0bcbee7f157a45cc6c69877396d113774
smjoy222 <smjoy222@gmail.com>
2025-03-28 19:02:30 +0600

    task3

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,34 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _displayText = "Click The Button";
-  void _changeText(){
-    setState(() {
-      _displayText = "Button pressed";
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                _displayText,
-                style: TextStyle(fontSize: 30),
+ 
+ void main() {
+   runApp(const MyApp());
+ }
+ 
+ class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: Scaffold(
+         appBar: AppBar(
+           centerTitle: true,
+           title: Text("The Tracker App"),
+           backgroundColor: const Color.fromARGB(255, 255, 77, 77),
+         ),
+         body: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Center(
+                child: Text("Item Number: $index",selectionColor: Color.fromARGB(0, 16, 164, 36),),
+              
               ),
-              ElevatedButton(onPressed: _changeText, child: Text("Press")),
-            ],
-          ),
+            );
+          },
         ),
-      ),
-    );
-  }
-}
+       ),
+     );
+   }
+ }
