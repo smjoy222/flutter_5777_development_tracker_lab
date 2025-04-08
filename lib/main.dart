c0dc1152bbfb04386386fd52f84090046098961d
smjoy222 <smjoy222@gmail.com>
2025-04-08 23:56:19 +0600

    task14

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,89 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
+  
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      title: 'Tracker App - Task 14',
+      theme: ThemeData.dark(),
+      home: SwipeListScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class SwipeListScreen extends StatefulWidget {
+  @override
+  _SwipeListScreenState createState() => _SwipeListScreenState();
+}
+
+class _SwipeListScreenState extends State<SwipeListScreen> {
+  List<String> items = List.generate(15, (index) => 'Item ${index + 1}');
 
   @override
   Widget build(BuildContext context) {
+  
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 25, 196, 6),
-        elevation: 100,
-        title: Row(
-          children: [
-            Image.asset("assets/images/biglogo.jpg", width: 40, height: 40),
-            Text(
-              "  Bazar Sadaai App",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(icon: Icon(Icons.search), onPressed: () {}),
-          IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-        ],
+        
+        title: Text('Tracker App - Swap to Edit/Delete',),
+        titleTextStyle: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 3, 215, 215),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment:
-              CrossAxisAlignment.center, // ensures horizontal centering
-          children: [
-            Image.asset("assets/images/biglogo.jpg", width: 300, height: 300),
-            SizedBox(height: 16), // optional spacing
-            Text(
-              "Home Screen",
-              textAlign: TextAlign.center, // centers text content
-              style: TextStyle(
-                fontSize: 45,
-                color: Color.fromARGB(255, 5, 196, 21),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          return Dismissible(
+            key: Key(item),
+            background: Container(
+              color: Colors.blue,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+
+              child: Icon(Icons.edit, color: Colors.white, size: 30),
+            ),
+            secondaryBackground: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Icon(Icons.delete, color: Colors.white, size: 30),
+            ),
+            onDismissed: (direction) {
+              if (direction == DismissDirection.endToStart) {
+                setState(() {
+                  items.removeAt(index);
+                });
+
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text('$item deleted')));
+              } else if (direction == DismissDirection.startToEnd) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text('Edit $item')));
+              }
+            },
+            child: Card(
+              elevation: 2,
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: ListTile(
+                title: Text(item),
+                //leading: Icon(Icons.list),
               ),
             ),
-            SizedBox(height: 8), // optional spacing
-            Text(
-              "Customize App Bar, Logo, Title, Action Button",
-              textAlign: TextAlign.center, // centers text content
-              style: TextStyle(
-                fontSize: 27,
-                color: Color.fromARGB(255, 55, 74, 57),
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
