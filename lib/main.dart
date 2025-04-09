499c0e5bb1662bf1b51f8c13bca0afae2f0ba651
smjoy222 <smjoy222@gmail.com>
2025-04-09 15:26:43 +0600

    task16

--- lib/main.dart
+++ lib/main.dart
@@ -7,99 +7,60 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Date & Time Picker',
-      theme: ThemeData.dark(),
-      home: const DateTimePickerScreen(),
+      title: 'Tracker App - Task 16',
+      home: AnimatedContainerExample(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
     );
   }
 }
 
-class DateTimePickerScreen extends StatefulWidget {
-  const DateTimePickerScreen({super.key});
+class AnimatedContainerExample extends StatefulWidget {
   @override
-  DateTimePickerScreenState createState() => DateTimePickerScreenState();
+  _AnimatedContainerExampleState createState() =>
+      _AnimatedContainerExampleState();
 }
 
-class DateTimePickerScreenState extends State<DateTimePickerScreen> {
-  DateTime? selectedDate;
-  TimeOfDay? selectedTime;
-
-  Future<void> _pickDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: selectedDate ?? DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime(2500),
-    );
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
-  Future<void> _pickTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: selectedTime ?? TimeOfDay.now(),
-    );
-    if (picked != null && picked != selectedTime) {
-      setState(() {
-        selectedTime = picked;
-      });
-    }
-  }
+class _AnimatedContainerExampleState extends State<AnimatedContainerExample>{
+  bool _isChanged=false;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Pick Date & Time App'),
-        titleTextStyle: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
+        title: Text('Animated Container App'),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => _pickDate(context),
+      
+      body: Center(
+        child: AnimatedContainer(
+          duration:Duration(seconds: 1),
+          curve:Curves.easeInOut,
+          width: _isChanged ? 200 : 100,
+          height: _isChanged ?100:200,
 
-                child: const Text('Pick a Date'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => _pickTime(context),
-                child: const Text('Pick Time'),
-              ),
-              const SizedBox(height: 40),
-              Text(
-                selectedDate == null
-                    ? 'No Date Selected Yet'
-                    : 'Selected Date: ${selectedDate?.toLocal()}',
-                style: const TextStyle(fontSize: 18),
-              ),
-              Text(
-                selectedTime == null
-                    ? 'No Time Selected Yet'
-                    : 'Selected Time: ${selectedTime?.format(context)}',
-                style: const TextStyle(fontSize: 18),
-              ),
-            ],
-          ),
-        ),
+          decoration: BoxDecoration(
+            color: _isChanged? const Color.fromARGB(255, 255, 0, 0) :Colors.blue,
+            borderRadius: BorderRadius.circular(_isChanged?30 :0),
+                      ),
       ),
+      ),
+      floatingActionButton:FloatingActionButton(
+        onPressed: (){
+          setState(() {
+          _isChanged=!_isChanged;
+          });
+        },
+        child: Icon(Icons.play_arrow),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      )
     );
   }
 }
