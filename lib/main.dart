1fdde4566e0f0cda8fb11bc9e94b2fecb940a3cf
smjoy222 <smjoy222@gmail.com>
2025-04-09 16:11:32 +0600

    task17

--- lib/main.dart
+++ lib/main.dart
@@ -4,63 +4,57 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = true; 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tracker App - Task 16',
-      home: AnimatedContainerExample(),
+      title: 'Tracker App - Task 17',
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: SettingsScreen(
+          isDarkMode: _isDarkMode,
+          onThemeChanged: (bool value) {
+            setState(() {
+              _isDarkMode = value;
+            });
+          }),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
     );
   }
 }
 
-class AnimatedContainerExample extends StatefulWidget {
-  @override
-  _AnimatedContainerExampleState createState() =>
-      _AnimatedContainerExampleState();
-}
+class SettingsScreen extends StatelessWidget {
+  final bool isDarkMode;
+  final ValueChanged<bool> onThemeChanged;
+  const SettingsScreen({
+    super.key,
+    required this.isDarkMode,
+    required this.onThemeChanged,
+  });
 
-class _AnimatedContainerExampleState extends State<AnimatedContainerExample>{
-  bool _isChanged=false;
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Animated Container App'),
-        backgroundColor: Colors.blue,
+        title: const Text('Settings'),
         centerTitle: true,
       ),
-      
-      body: Center(
-        child: AnimatedContainer(
-          duration:Duration(seconds: 1),
-          curve:Curves.easeInOut,
-          width: _isChanged ? 200 : 100,
-          height: _isChanged ?100:200,
-
-          decoration: BoxDecoration(
-            color: _isChanged? const Color.fromARGB(255, 255, 0, 0) :Colors.blue,
-            borderRadius: BorderRadius.circular(_isChanged?30 :0),
-                      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Dark Mode'),
+            trailing: Switch(value: isDarkMode, onChanged: onThemeChanged),
+          )
+        ],
       ),
-      ),
-      floatingActionButton:FloatingActionButton(
-        onPressed: (){
-          setState(() {
-          _isChanged=!_isChanged;
-          });
-        },
-        child: Icon(Icons.play_arrow),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      )
     );
   }
 }
